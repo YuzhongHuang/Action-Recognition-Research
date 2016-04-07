751063187c2ef0a961adf5ca33f6eb81ce352793
YuzhongHuang <yuzhong.huang@students.olin.edu>
2016-04-07 14:23:08 -0400

    use nngraph to visualize a LeNet

---
+++ LeNet_graph.lua
@@ -0,0 +1,38 @@
+require "nngraph"
+
+-- I will start with the LeNet I build with NN to visualize my network
+
+-- define the LeNet function
+
+function get_LeNet()
+    -- it is common style to mark inputs with identity nodes for clarity.
+    local input = nn.Identity()()
+    
+    -- each hidden layer is achieved by connecting the previous one
+    -- here we define a single hidden layer network
+    local conv1 = nn.SpatialConvolution(3, 6, 5, 5)(input) -- 3 input image channels, 6 output channels, 5x5 convolution kernel
+    local conv1_a = nn.ReLU()(conv1) -- non-linearity 
+    local pool1 = nn.SpatialMaxPooling(2,2,2,2)(conv1_a) -- A max-pooling operation that looks at 2x2 windows and finds the max.
+
+    local conv2 = nn.SpatialConvolution(6, 16, 5, 5)(pool1) -- 6 input image channels, 16 output channels, 5x5 convolution kernel
+    local conv2_a = nn.ReLU()(conv2) -- non-linearity 
+    local pool2 = nn.SpatialMaxPooling(2,2,2,2)(conv2_a) -- A max-pooling operation that looks at 2x2 windows and finds the max.
+   
+    local reshape = nn.View(16*5*5)(pool2) -- reshapes from a 3D tensor of 16x5x5 into 1D tensor of 16*5*5
+
+    local fullConnect1 = nn.Linear(16*5*5, 120)(reshape) -- fully connected layer (matrix multiplication between input and weights)
+    local fullConnect1_a = nn.ReLU()(fullConnect1) -- non-linearity 
+    
+    local fullConnect2 = nn.Linear(120, 84)(fullConnect1_a)
+    local fullConnect2_a = nn.ReLU()(fullConnect2) -- non-linearity 
+    
+    local fullConnect3 = nn.Linear(84, 10)(fullConnect2_a) -- 10 is the number of outputs of the network (in this case, 10 digits)
+    local softMax = nn.LogSoftMax()(fullConnect3)
+    
+    -- the following function call inspects the local variables in this
+    -- function and finds the nodes corresponding to local variables.
+    nngraph.annotateNodes()
+    return nn.gModule({input}, {softMax})
+end
+    
+LeNet = get_LeNet()
